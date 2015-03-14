b68b106541958c5e5077738cbf048366de157966
Thilo Cestonaro <thilo@cestona.ro>
2015-03-14 15:34:21 +0100

    added define for addons file

--- modules/defines.py
+++ modules/defines.py
@@ -2,3 +2,5 @@ from PyQt4 import Qt
 
 WOW_FOLDER_KEY = "Preferences/wowfolder"
 WOW_FOLDER_DEFAULT = "%s/.wine/drive_c/Program Files (x86)/World of Warcraft" % (Qt.QDir.homePath()) 
+
+LCURSE_ADDONS = "%s/.lcurse/addons.json" % (Qt.QDir.homePath())
