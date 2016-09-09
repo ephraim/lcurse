2b5a4d9a25286e6705a4ad5215e9ee2ac44bbde7
boombatower <jimmy@boombatower.com>
2016-09-08 22:42:32 -0500

    Remove extraneous percent signs from progress text.
    See https://github.com/ephraim/lcurse/issues/35.

--- modules/waitdlg.py
+++ modules/waitdlg.py
@@ -305,7 +305,7 @@ class UpdateCatalogDlg(Qt.QDialog):
     def onProgress(self, foundAddons):
         value = self.progress.value() + 1
         self.progress.setValue(value)
-        self.progress.setFormat(self.tr("%%p%% - found Addons: {}").format(foundAddons))
+        self.progress.setFormat(self.tr("%p% - found Addons: {}").format(foundAddons))
 
     @Qt.pyqtSlot(Qt.QVariant)
     def onUpdateCatalogFinished(self, addons):
