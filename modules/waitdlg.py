37d45e38d2c89b67457de5bcd4ceb65243fbd130
Nicolas Dumazet <nicdumz.commits@gmail.com>
2017-01-08 12:19:55 +0100

    General cleanups. No code logic changes intended.
    - re.match or re.search returns object that bool() to True if there are
      results, so `if re.match` does the same as `if re.match(..) != None`
    - lists evaluate to True if they have an element, so `if l` is the same
      as `if len(l) > 0`
    - str objects are lists, so `if s` is the same as `if len(s) > 0`, or
      `if s != ""`
    - `if obj` is generally a superset of `if obj != None`
    - dir is a reserved keyword, don't use it as a variable
    - in two cases in applications.py, return early to reduce indentation

--- modules/waitdlg.py
+++ modules/waitdlg.py
@@ -155,10 +155,10 @@ class CheckWorker(Qt.QThread):
             if self.addon[4]:
                 possibleValues = re.compile("^[12]$")
             lis = soup.findAll("td", attrs={"data-sort-value": possibleValues})
-            if len(lis) > 0:
+            if lis:
                 versionIdx = 0
                 version = lis[versionIdx].parent.contents[0].contents[0].string
-                if len(lis) > 1 and pattern.search(version) != None and pattern.sub("", version) == \
+                if len(lis) > 1 and pattern.search(version) and pattern.sub("", version) == \
                         lis[1].parent.contents[0].contents[0].string:
                     versionIdx = 1
                     version = lis[versionIdx].parent.contents[0].contents[0].string
@@ -181,7 +181,7 @@ class CheckWorker(Qt.QThread):
         elif self.addon[2].endswith(".git"):
             result = self.needsUpdateGit()
 
-        if result != None:
+        if result:
             self.checkFinished.emit(self.addon, result[0], result[1])
         else:
             self.checkFinished.emit(self.addon, False, False)
@@ -335,7 +335,7 @@ class UpdateCatalogWorker(Qt.QThread):
         lastpage = 1
         if page == 1:
             pager = soup.select("ul.b-pagination-list.paging-list.j-tablesorter-pager.j-listing-pagination li")
-            if len(pager) > 0:
+            if pager:
                 lastpage = int(pager[len(pager) - 2].contents[0].contents[0])
 
         links = soup.select("li .title h4 a")  # li .title h4 a")
